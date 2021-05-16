b3ccd99ebadadb30f7d67df772867726c3d7717d
jonas-kgomo <jonaskgmoo@gmail.com>
2021-05-16 05:14:51 +0300

    status bar

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,23 +1,35 @@
 import React from "react";
 
-
 export const Footer: React.FC = () => {
   return (
     <footer className="bottom-0  w-full mb-12  items-center justify-between mx-auto container py-9 px-5">
-     
       <div className="grid grid-cols-4 gap-4 ">
-        <div><img src="/static/simplelogo.svg" className="w-12"/></div>
-        
+        <div>
+          <img src="/static/simplelogo.svg" className="w-12" />
+        </div>
+
         <ul className="space-y-4">
-          <p className="text-base font-bold text-gray tracking-wide uppercase">company</p>  
+          <p className="text-base font-bold text-gray tracking-wide uppercase">
+            company
+          </p>
           <li>Pricing</li>
           <li>Blog</li>
-          <li>Status <p className="text-indigo-300 text-">operational</p></li>
-          <li><a href="/assets">Assets</a></li>
+          <li>
+            Status
+            <p className="inline-flex mx-2 px-3 text-center w-16 h-8 rounded-md border border-1 border-lightGray shadow-sm bg-snow ">
+              beta
+            </p>
+            {/* <p className="text-indigo-300 text-">operational</p> */}
+          </li>
+          <li>
+            <a href="/assets">Assets</a>
+          </li>
         </ul>
-        
+
         <ul className="space-y-4">
-          <p className="text-base font-bold text-gray tracking-wide uppercase">legal</p>  
+          <p className="text-base font-bold text-gray tracking-wide uppercase">
+            legal
+          </p>
           <li>Privacy Policy</li>
           <li>Apache License</li>
           <li>Trademark Policy</li>
@@ -25,25 +37,31 @@ export const Footer: React.FC = () => {
         </ul>
 
         <ul className="space-y-4">
-          <p className="text-base block font-bold text-gray tracking-wide uppercase">social</p>
-          <li><a href="https://twitter.com/affordancehq">Twitter</a></li>
-          <li><a href="https://github.com/Affordance-App/affordance">Github</a></li>
-          <li><a href="https://discord.gg/NhfngzZX"> Discord</a></li>
+          <p className="text-base block font-bold text-gray tracking-wide uppercase">
+            social
+          </p>
+          <li>
+            <a href="https://twitter.com/affordancehq">Twitter</a>
+          </li>
+          <li>
+            <a href="https://github.com/Affordance-App/affordance">Github</a>
+          </li>
+          <li>
+            <a href="https://discord.gg/NhfngzZX"> Discord</a>
+          </li>
         </ul>
-        
+      </div>
 
-       </div>
-      
-    
       <div className="small flex justify-center  mt-3 text-gray items-center  mx-auto">
-         <p>Copyright © 2021 Affordance All rights reserved.{" "}</p> 
-           By
-           <a href="https://twitter.com/coderinblack" className="text-black">
-          @codeinblack 
-          </a>
-          <p>and</p>
-          <a href="https://twitter.com/jonas_kg" className="text-black">
-          @jonas</a> 
+        <p>Copyright © 2021 Affordance All rights reserved. </p>
+        By
+        <a href="https://twitter.com/coderinblack" className="text-black">
+          @codeinblack
+        </a>
+        <p>and</p>
+        <a href="https://twitter.com/jonas_kg" className="text-black">
+          @jonas
+        </a>
       </div>
     </footer>
   );
