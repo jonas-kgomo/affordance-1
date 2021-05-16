39716e6932201dc6f4192ef235194a43dfad6e1b
jonas-kgomo <jonaskgmoo@gmail.com>
2021-05-16 05:12:49 +0300

    hover flex

--- components/Hover.tsx
+++ components/Hover.tsx
@@ -23,32 +23,41 @@ opacity-0 invisible dropdown-menu transition-all duration-300 transform origin-t
       To: "transform opacity-0 scale-95"
        */}
       <div className="absolute right-0 md:order-top-left transform shadow-sm  w-52 mt-2 origin-top-right  border border-lightGray rounded-lg bg-white text-darkGray focus:outline-none">
-        <div className="py-1 px-4 text-overflow mx-auto w-full text-left  text-darkgray">
+        <div className="py-1 text-overflow mx-auto w-full text-left  text-darkgray">
           {/* Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" */}
           <a
-            href="#"
-            className=" active:bg-gray block px-4 py-2 "
+            href="/settings"
+            className=" hover:bg-snow hover:border-darkGray block px-4 py-2 "
             role="menuitem"
             id="menu-item-0"
           >
-            Account settings
+            Settings
           </a>
-          <a href="#" className=" preblock px-4 py-2 ">
+          <a
+            href="#"
+            className="hover:bg-snow hover:border-darkGray  block preblock px-4 py-2 "
+          >
             Support
           </a>
-          <a href="#" className=" block px-4 py-2 ">
+          <a
+            href="#"
+            className="hover:bg-snow hover:border-darkGray  block px-4 py-2 "
+          >
             License
           </a>
-          <a href="#" className=" flex px-4 py-2 ">
+          <a
+            href="#"
+            className="hover:bg-snow hover:border-darkGray  flex px-4 py-2 "
+          >
             Theme{" "}
-            <Button className="inline-flex mx-4 px-3 text-center w-16 h-8 rounded-md border border-1 border-snow shadow-sm bg-white ">
+            <Button className="inline-flex mx-4 px-3 text-center w-16 h-8 rounded-md border border-1 border-lightGray shadow-sm bg-snow ">
               Solar
             </Button>
           </a>
           <form method="POST" action="#" role="none">
             <button
               type="submit"
-              className="text-gray =block w-full text-left px-4 py-2 "
+              className="hover:bg-snow hover:border-darkGray text-gray block w-full text-left px-4 py-2 "
             >
               Sign out
             </button>
